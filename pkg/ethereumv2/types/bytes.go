92ae305e2fa65adff5ba8508c106746fafd46cc7
Michał Dobaczewski <mdobak@gmail.com>
2022-10-08 04:38:01 +0200

    Add license header

--- pkg/ethereumv2/types/bytes.go
+++ pkg/ethereumv2/types/bytes.go
@@ -1,8 +1,24 @@
+//  Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package types
 
 // Bytes represents a byte slice.
 type Bytes []byte
 
+// HexToBytes parses a hex string into a Bytes.
 func HexToBytes(hex string) Bytes {
 	b := Bytes{}
 	_ = b.UnmarshalText([]byte(hex))
