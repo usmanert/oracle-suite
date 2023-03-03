f1a0105fd578202736b8e40b6f6d2452c5308fed
abbasidd <45910850+abbasidd@users.noreply.github.com>
2023-03-03 12:49:33 +0500

    change the gsu rate api (#8)

--- pkg/price/provider/origins/gsu.go
+++ pkg/price/provider/origins/gsu.go
@@ -16,145 +16,129 @@
 package origins
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/chronicleprotocol/oracle-suite/pkg/util/query"
-	"math/big"
-	"time"
-
-	"strings"
+        "encoding/json"
+        "fmt"
+        "github.com/chronicleprotocol/oracle-suite/pkg/util/query"
+        "time"
+        "strconv"
+        "strings"
 )
 
 // GSU URL
-const gsuURL = "https://goerli.gsu.io/Umbraco/Api/Rates/GSU/?symbol=%s"
+const gsuURL = "https://api.gsucoin.app/Products/GSULive/?symbol=%s"
 const wad = 1000000000000000000
 
 type gsuResponse struct {
-	Price     string    `json:"price"`
-	Ask       string    `json:"ask"`
-	Bid       string    `json:"bid"`
-	Volume    string    `json:"volume"`
-	Symbol    string    `json:"symbol"`
+        Price     string    `json:"price"`
+        Ask       string    `json:"ask"`
+        Bid       string    `json:"bid"`
+        Volume    string    `json:"volume"`
+        Symbol    string    `json:"symbol"`
 }
 
 // GSU exchange handler
 type GSU struct {
-	WorkerPool query.WorkerPool
+        WorkerPool query.WorkerPool
 }
 
 func (h *GSU) localPairName(pair Pair) string {
-	return strings.ToUpper(pair.Base)
+        return strings.ToUpper(pair.Base)
 }
 
 func (h *GSU) getURL(pairs []Pair) string {
-	pairsStr := make([]string, len(pairs))
-	for i, pair := range pairs {
-		pairsStr[i] = h.localPairName(pair)
-	}
-	return fmt.Sprintf(gsuURL, strings.Join(pairsStr, ","))
+        pairsStr := make([]string, len(pairs))
+        for i, pair := range pairs {
+                pairsStr[i] = h.localPairName(pair)
+        }
+        return fmt.Sprintf(gsuURL, strings.Join(pairsStr, ","))
 }
 
 func (h GSU) Pool() query.WorkerPool {
-	return h.WorkerPool
+        return h.WorkerPool
 }
 
 func (h GSU) PullPrices(pairs []Pair) []FetchResult {
-	crs, err := h.fetch(pairs)
-	if err != nil {
-		return fetchResultListWithErrors(pairs, err)
-	}
-	return crs
+        crs, err := h.fetch(pairs)
+        if err != nil {
+                return fetchResultListWithErrors(pairs, err)
+        }
+        return crs
 }
 
 func (h *GSU) fetch(pairs []Pair) ([]FetchResult, error) {
 
-	crs := make([]FetchResult, len(pairs))
-	respMap := map[string]gsuResponse{}
-	for i, pair := range pairs {
-		symbol := h.localPairName(pair)
-		url := fmt.Sprintf(gsuURL, symbol)
-		req := &query.HTTPRequest{
-			URL: url,
-		}
-		// make query
-		res := h.Pool().Query(req)
-		if res == nil {
-			return nil, ErrEmptyOriginResponse
-		}
-		if res.Error != nil {
-			return nil, res.Error
-		}
-		// parsing JSON
-		var resp gsuResponse
-		err := json.Unmarshal(res.Body, &resp)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse gsu response: %w", err)
-		}
-		respMap[symbol] = resp
+        crs := make([]FetchResult, len(pairs))
+        respMap := map[string]gsuResponse{}
+        for i, pair := range pairs {
+                symbol := h.localPairName(pair)
+                url := fmt.Sprintf(gsuURL, symbol)
+                req := &query.HTTPRequest{
+                        URL: url,
+                }
+                // make query
+                res := h.Pool().Query(req)
+                if res == nil {
+                        return nil, ErrEmptyOriginResponse
+                }
+                if res.Error != nil {
+                        return nil, res.Error
+                }
+                // parsing JSON
+                var resp gsuResponse
+                err := json.Unmarshal(res.Body, &resp)
+                if err != nil {
+                        return nil, fmt.Errorf("failed to parse gsu response: %w", err)
+                }
+                respMap[symbol] = resp
 
-		if resp, has := respMap[pair.Base]; has {
-			p, err := h.newPrice(pair, resp)
-			if err != nil {
-				crs[i] = fetchResultWithError(
-					pair,
-					fmt.Errorf("failed to create price point from gsu response: %w: %s", err, res.Body),
-				)
-			} else {
-				crs[i] = fetchResult(p)
-			}
-		} else {
-			crs[i] = fetchResultWithError(
-				pair,
-				fmt.Errorf("failed to find symbol %s in gsu response: %s", pair, res.Body),
-			)
-		}
-	}
-	return crs, nil
+                if resp, has := respMap[pair.Base]; has {
+                        p, err := h.newPrice(pair, resp)
+                        if err != nil {
+                                crs[i] = fetchResultWithError(
+                                        pair,
+                                        fmt.Errorf("failed to create price point from gsu response: %w: %s", err, res.Body),
+                                )
+                        } else {
+                                crs[i] = fetchResult(p)
+                        }
+                } else {
+                        crs[i] = fetchResultWithError(
+                                pair,
+                                fmt.Errorf("failed to find symbol %s in gsu response: %s", pair, res.Body),
+                        )
+                }
+        }
+        return crs, nil
 }
 
 func (h *GSU) newPrice(pair Pair, resp gsuResponse) (Price, error) {
-	e := new(big.Float).SetUint64(wad)
-	z := new(big.Int).SetUint64(0)
-
-	// Parsing price from string.
-	v, ok := new(big.Int).SetString(resp.Price,10)
-	if !ok {
-		return Price{}, fmt.Errorf("failed to parse price from gsu exchange")
-	}
-	f := new(big.Float).SetInt(v)
-	price, _ := f.Quo(f, e).Float64()
-
-	//ask
-	v, ok = new(big.Int).SetString(resp.Ask,10)
-	ask := float64(0)
-	if ok && v.Cmp(z) != 0 {
-		f = new(big.Float).SetInt(v)
-		ask, _ = f.Quo(f, e).Float64()
-	}
-
-	//bid
-	v, ok = new(big.Int).SetString(resp.Bid,10)
-	bid := float64(0)
-	if ok && v.Cmp(z) != 0 {
-		f = new(big.Float).SetInt(v)
-		bid, _ = f.Quo(f, e).Float64()
-	}
-
-	//vol
-	v, ok = new(big.Int).SetString(resp.Bid,10)
-	vol := float64(0)
-	if ok && v.Cmp(z) != 0 {
-		f = new(big.Float).SetInt(v)
-		vol, _ = f.Quo(f, e).Float64()
-	}
-
-	// Building Price.
-	return Price{
-		Pair:      pair,
-		Price:     price,
-		Ask:       ask,
-		Bid:       bid,
-		Volume24h: vol,
-		Timestamp: time.Now(),
-	}, nil
+        // Parsing price from string.
+        price, err := strconv.ParseFloat(resp.Price, 64)
+        if err != nil {
+                return Price{}, fmt.Errorf("failed to parse price from hitbtc exchange")
+        }
+        // Parsing ask from string.
+        ask, err := strconv.ParseFloat(resp.Ask, 64)
+        if err != nil {
+                return Price{}, fmt.Errorf("failed to parse ask from hitbtc exchange")
+        }
+        // Parsing volume from string.
+        volume, err := strconv.ParseFloat(resp.Volume, 64)
+        if err != nil {
+                return Price{}, fmt.Errorf("failed to parse volume from hitbtc exchange")
+        }
+        // Parsing bid from string.
+        bid, err := strconv.ParseFloat(resp.Bid, 64)
+        if err != nil {
+                return Price{}, fmt.Errorf("failed to parse bid from hitbtc exchange")
+        }
+        // Building Price.
+        return Price{
+                Pair:      pair,
+                Price:     price,
+                Ask:       ask,
+                Bid:       bid,
+                Volume24h: volume,
+                Timestamp: time.Now(),
+        }, nil
 }
